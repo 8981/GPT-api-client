21f01ff74d739253755de71b67dcb9cb127e56c7
Artem Prokofiev <artem-prokofiev@yandex.ru>
2023-01-23 22:15:06 +0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# GPT-api-client
+# GPT3-api-client
+>Use the OpenAI GPT-3 API to create “ChatGPT-like” .NET command line tool
+
+### 🔗 OpenAPI Docs: https://beta.openai.com/docs/introduction
+### 🔗 GPT-3 Tool: https://gpttools.com/comparisontool
+### 🔗 Single File Apps: https://learn.microsoft.com/en-us/dotnet/core/deploying/single-file/overview?WT.mc_id=DX-MVP-5004571&tabs=cli
---
+++ askMe/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Security.AccessControl;
+using System.Text;
+using Newtonsoft.Json;
+
+if (args.Length > 0)
+{
+    HttpClient client = new HttpClient();
+
+    client.DefaultRequestHeaders.Add("authorization", "Bearer <INSERT YOUR API KEY HERE>");
+
+    //Example Request body - https://beta.openai.com/docs/api-reference/completions/create#:~:text=Learn%20more.-,Example%20request,-text%2Ddavinci%2D003
+    var content = new StringContent("{\"model\": \"text-davinci-001\", \"prompt\": \""+ args[0] +"\",\"temperature\": 1,\"max_tokens\": 100}",
+        Encoding.UTF8, "application/json");
+
+    HttpResponseMessage response = await client.PostAsync("https://api.openai.com/v1/completions", content);
+
+    string responseString = await response.Content.ReadAsStringAsync();
+
+    try
+    {
+        var dyData = JsonConvert.DeserializeObject<dynamic>(responseString);
+        
+        string guess = GuessCommand(dyData!.choices[0].text);
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($"---> My guess at the command prompt is: {guess}");
+        Console.ResetColor();
+
+    }
+    catch(Exception ex)
+    {
+        Console.WriteLine($"---> Could not deserialize the JSON: {ex.Message}");
+    }
+
+    //Console.WriteLine(responseString);
+}
+else
+{
+    Console.WriteLine("---> You need to provide some input");
+}
+
+static string GuessCommand(string raw)
+{
+    Console.WriteLine("---> GPT-3 API Returned Text:");
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine(raw);
+
+    var lastIndex = raw.LastIndexOf('\n');
+    string guess = raw.Substring(lastIndex + 1);
+
+    Console.ResetColor();
+
+    TextCopy.ClipboardService.SetText(guess);
+
+    return guess;
+}
